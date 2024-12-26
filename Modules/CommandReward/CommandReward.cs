287a562c1962f57edbbfca4c663d0147ac60b65a
Гребенщиков Андрей <a-grebenshikov@bk.ru>
2024-12-26 15:49:10 +0500

    Добавлен универсальный модуль для выдачи награды в виде консольной команды, удален KickInvite

---
+++ Modules/CommandReward/CommandReward.cs
@@ -0,0 +1,54 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Capabilities;
+using Invites.API;
+
+namespace CommandReward;
+
+public class Constants
+{
+    public const string PluginName = "Command Reward [Module]";
+    public const string PluginVersion = "1.0.0";
+    public const string PluginAuthor = "Xummuk97";
+
+    public const string RewardId = "Command";
+}
+
+public class RewardDelegate : IRewardDelegate
+{
+    public void OnGive(CCSPlayerController controller, string? @params)
+    {
+        var command = @params ?? "";
+
+        command = command.Replace("{userid}", controller.UserId.ToString());
+        command = command.Replace("{name}", controller.PlayerName);
+        command = command.Replace("{steamid2}", controller.AuthorizedSteamID?.SteamId2);
+        command = command.Replace("{steamid3}", controller.AuthorizedSteamID?.SteamId3);
+        command = command.Replace("{steamid32}", controller.AuthorizedSteamID?.SteamId32.ToString());
+        command = command.Replace("{steamid64}", controller.AuthorizedSteamID?.SteamId64.ToString());
+
+        if (!string.IsNullOrEmpty(command))
+        {
+            Server.ExecuteCommand(command);
+        }
+    }
+}
+
+public class CommandReward : BasePlugin
+{
+    public override string ModuleName => Constants.PluginName;
+    public override string ModuleVersion => Constants.PluginVersion;
+    public override string ModuleAuthor => Constants.PluginAuthor;
+
+    private PluginCapability<IAPI> _apiCapability = new(Invites.API.Constants.APICapability);
+
+    public static IAPI? API { get; private set; }
+
+    public override void OnAllPluginsLoaded(bool hotReload)
+    {
+        base.OnAllPluginsLoaded(hotReload);
+
+        API = _apiCapability.Get();
+        API?.AddReward(Constants.RewardId, new RewardDelegate());
+    }
+}
